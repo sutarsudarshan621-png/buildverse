b88ffe34ec4fb85744cdb878653d5f41bd50d66e
Sudhakar Sutar <sudhakarsutar101@gmail.com>
2026-06-14 22:35:46 +0530

    all setup for the frontend , backend, smartcontract is done!

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+D:\buildVerse\backend\target\debug\backend.exe: D:\buildVerse\backend\src\main.rs
---
+++ backend/target/debug/deps/backend.d
@@ -0,0 +1,5 @@
+D:\buildVerse\backend\target\debug\deps\backend.d: src\main.rs
+
+D:\buildVerse\backend\target\debug\deps\backend.exe: src\main.rs
+
+src\main.rs:
